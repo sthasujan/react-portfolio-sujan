dbbf19a7c2de2a9b90a2b66c3f078d7067044d65
Sujan Shrestha <sujan.spy007@gmail.com>
2024-07-10 19:40:52 +1000

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+import project1 from "../assets/projects/sujanPro1.jpg";
+import project2 from "../assets/projects/sujanPro2.jpg";
+import project3 from "../assets/projects/sujanPro3.jpg";
+import project4 from "../assets/projects/sujanPro4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with 5 years of hands-on experience, specializing in crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies including Node.js, ASP.NET Core, SQL Server, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `Driven by dedication and versatility, I am a passionate full stack developer committed to creating efficient and intuitive web applications. Over 5 years of professional experience, I have adeptly utilized a diverse range of technologies such as React, Next.js, Node.js, ASP.NET Core, ASP.NET MVC, SQL Server, MySQL, PostgreSQL, and MongoDB. My journey in web development began fueled by a profound curiosity about system mechanics, evolving into a career where I continually embrace new challenges and learning opportunities. I thrive in collaborative environments, deriving satisfaction from solving intricate problems to deliver top-tier solutions. Beyond coding, I maintain an active lifestyle, explore emerging technologies, and contribute to open-source initiatives.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Freelance Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2021 - 2023",
+    role: "Information Technology Officer",
+    company: "Sukuna Secondary School",
+    description: `Works as an IT consultant for the betterment of digital literacy and improved quality education, School website development and maintenance, System Admin`,
+    technologies: ["Office 365","HTML", "CSS", "PHP", "mySQL", "Canva" ],
+  },
+  {
+    year: "2019 - 2021",
+    role: "Software Engineer",
+    company: "Dailo Technologies",
+    description: `Developed and maintained web applications using JavaScript, ASP.NET Core, React.js and SQL Server. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["ASP.NET Core", "React.js", "MVC Framework", "JavaScript", "SQL Server"],
+  },
+  {
+    year: "2018 - 2019",
+    role: "Associate Software Engineer",
+    company: "InfoKoNN Technologies Pvt. Ltd.",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and ASP.NET Core. Managed databases and implemented data storage solutions using SQL Server. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["JavaScript", "React.js", "ASP.NET Core", "SQL Server"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Mernda, Victoria, Vic 3754",
+  phoneNo: "+61 0467 451 429 ",
+  email: "me@example.com",
+};
